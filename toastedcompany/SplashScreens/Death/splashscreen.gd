51f0aa4f7825455267ebbad5f9338fd199db8ee1
HydroshieldMKII <114195376+HydroshieldMKII@users.noreply.github.com>
2024-11-24 17:53:30 -0500

    Fixed death screen

--- toastedcompany/SplashScreens/Death/splashscreen.gd
+++ toastedcompany/SplashScreens/Death/splashscreen.gd
@@ -1,11 +1,16 @@
 extends CanvasLayer
 
-@onready var try_again_btn: Button = $VBoxContainer/TryAgain
-@onready var menu_btn: Button = $VBoxContainer/MainMenu
-@onready var exit_btn: Button = $VBoxContainer/Exit
+@onready var try_again_btn: Button = $HBoxContainer/VBoxContainer/TryAgain
+@onready var menu_btn: Button = $HBoxContainer/VBoxContainer/MainMenu
+@onready var other_btn: Button = $HBoxContainer/VBoxContainer/TryOther
+@onready var exit_btn: Button = $HBoxContainer/VBoxContainer/Exit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if DongeonGlobal.insane_mode:
+		other_btn.text = "Try Normal Mode"
+	else:
+		other_btn.text = "Try Insane Mode"	
 	try_again_btn.grab_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,9 +19,12 @@ func _process(delta: float) -> void:
 
 func _on_try_again_pressed() -> void:
 	# Get dongeon scene and reload it while closing the current scene
-	get_tree().change_scene_to_file("res://Dongeon/dongeon.tscn")
-	
+	get_tree().change_scene_to_file("res://SplashScreens/Loading/splashscreen.tscn")
 
+func _on_try_other_pressed() -> void:
+	DongeonGlobal.insane_mode = !DongeonGlobal.insane_mode
+	get_tree().change_scene_to_file("res://SplashScreens/Loading/splashscreen.tscn")
+	
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Menu.tscn")
 
