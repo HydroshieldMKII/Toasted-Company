b0c8f6dc48289999b998ee2ad362d6c409d98c4f
HydroshieldMKII <114195376+HydroshieldMKII@users.noreply.github.com>
2024-11-24 21:34:26 -0500

    Removed debug print

--- toastedcompany/menu.gd
+++ toastedcompany/menu.gd
@@ -13,7 +13,7 @@ extends Control
 		
 const SAVE_FILE_PATH: String = "user://high_scores.tres"
 const SAVE_FILE_NAME: String = "Highscore.tres"
-var high_score_data: HighScoreData = HighScoreData.new()
+var high_score_data: HighScoreData
 
 func _ready() -> void:
 	verify_save_dir(SAVE_FILE_PATH)
@@ -25,10 +25,8 @@ func verify_save_dir(path: String) -> void:
 	if FileAccess.file_exists(SAVE_FILE_PATH + SAVE_FILE_NAME):
 		high_score_data = ResourceLoader.load(SAVE_FILE_PATH + SAVE_FILE_NAME)
 		if high_score_data == null:
-			print("Saved object not found")
 			high_score_data = HighScoreData.new()
 	else:
-		print("Saved high score not found")
 		high_score_data = HighScoreData.new()
 	
 func update_labels() -> void:
@@ -36,13 +34,14 @@ func update_labels() -> void:
 	insane_high_score.text = "Highest Score (Insane): " + str(high_score_data.insane_high_score) + " (Level " + str(high_score_data.insane_level) + ")"
 	
 func set_scores(score: int, level: int, insane: bool) -> void:
+	verify_save_dir(SAVE_FILE_PATH) #Update object
+	
 	if insane:
 		high_score_data.set_insane_score(score)
 		high_score_data.set_insane_level(level)
 	else:
 		high_score_data.set_normal_score(score)
 		high_score_data.set_normal_level(level)
-		
 	ResourceSaver.save(high_score_data, SAVE_FILE_PATH + SAVE_FILE_NAME)
 
 func _on_start_btn_pressed() -> void:
